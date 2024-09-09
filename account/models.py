349d181e0b1fbcb0d350c284593415815ecdaa26
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-09-09 07:52:18 +0600

    Comments

--- account/models.py
+++ account/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     class Role(models.TextChoices):
+        """
+        User roles for providing permissions across applications.
+        """
         USER = 'user', 'User'
         OWNER = 'owner', 'Owner'
         EMPLOYEE = 'employee', 'Employee'
 
+    # Roles can be assigned only which are defined in Role class
+    # When a user register they are treated as normal user as they have
+    # user role assigned to them by default.
     role = models.CharField(
         max_length=10,
         choices=Role.choices,
